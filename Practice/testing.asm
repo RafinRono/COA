d5f3dd4e4b82f79f7821a3b5f7392fe1cbdec579
Rono <22-47226-1@student.aiub.edu>
2024-12-06 20:16:41 +0600

    update practice folder

---
+++ Practice/testing.asm
@@ -0,0 +1,56 @@
+.model SMALL  
+.STACK 100H
+
+.DATA
+A DB "Hello there!! $"  
+B DB "Input: $"
+C DB "Output: $"  
+;D DB -2  throws error    
+;D DW -2  works 
+D DW ?         
+E DB ?,?,?
+
+.CODE
+MAIN PROC
+MOV AX, @DATA      ;testin a commen
+MOV DS, AX
+    
+MOV AH, 9
+LEA DX, A         ; hello world
+INT 21H    
+
+MOV AH, 2
+MOV DL, 10     ; new line  pt1
+INT 21H
+
+MOV DL, 13      ; new line pt2
+INT 21H 
+          
+MOV AH, 9
+LEA DX, B          ;tells for input 1
+INT 21H 
+
+MOV AH, 1        ;input 1
+INT 21H
+MOV D, AX  
+
+MOV AH, 2
+MOV DL, 10     ; new line  pt1
+INT 21H
+
+MOV DL, 13      ; new line pt2
+INT 21H 
+          
+MOV AH, 9
+LEA DX, C         ;tells for output 1
+INT 21H 
+
+MOV AH, 2        ;output  1
+;MOV DX, 'A'       
+MOV DX, D
+INT 21H      
+  
+  MOV AH, 4CH
+  INT 21H
+  MAIN ENDP
+END
