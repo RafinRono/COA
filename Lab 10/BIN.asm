d929858b3347d142876cd1d7527c247493baacf5
Rono <22-47226-1@student.aiub.edu>
2025-01-19 20:52:47 +0600

    added bin input-output in lab10

---
+++ Lab 10/BIN.asm
@@ -0,0 +1,54 @@
+.model SMALL                                                                                                                                                       .model SMALL                ; means a small model
+.STACK 100H                 
+
+.DATA                       
+INPUT DB "INPUT: $"
+OUTPUT DB  0AH, 0DH,"OUTPUT $"
+
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX      
+    MOV AH, 9
+    LEA DX, INPUT
+    INT 21H   
+    XOR BX, BX 
+    MOV CX, 16
+    
+    WHILE_:
+    MOV AH, 1
+    INT 21H
+    CMP AL, 0DH
+    JE OUTPUT_
+    AND AL, 0FH
+    SHL BX, 1
+    OR BL, AL
+    LOOP WHILE_
+    
+    OUTPUT_:
+    MOV AH, 9
+    LEA DX, OUTPUT
+    INT 21H   
+    
+    MOV CX, 16
+    IF:
+    SHL BX, 1
+    JNC THEN
+    MOV AH, 2
+    MOV DL, 31H
+    INT 21H
+    JMP DISPLAY
+    
+    THEN:
+    MOV AH, 2
+    MOV DL, 30H
+    INT 21H
+    JMP DISPLAY
+    
+    DISPLAY:
+    LOOP IF
+    
+MOV AH, 4CH
+INT 21H
+MAIN ENDP
+END MAIN
